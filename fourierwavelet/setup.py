25004df2ce102e55b75481a5aac003c2ac179282
Archi20Rad <202103@edu.fa.ru>
2021-12-25 18:40:43 +0300

    0.0.3

--- fourierwavelet/setup.py
+++ fourierwavelet/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="fourierwavelet",
     packages=find_packages(),
-    version="0.0.1",
+    version="0.0.3",
     description='Ready-made Fourier transform and Wavelet from a csv file',
     author="Artem Radaykin",
     author_email="radaykin.artem@mail.ru",
     url="https://github.com/SMALA-comand/Fourier",
-    install_requires = ['matplotlib.pyplot','scipy.fft','csv','pywt','numpy'],
+    install_requires = ['matplotlib','scipy','pywt','numpy'],
     zip_safe=False, 
     classifiers=[
         "Programming Language :: Python :: 3.9",
